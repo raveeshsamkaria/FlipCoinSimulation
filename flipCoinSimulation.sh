a2b1b6d2368ddbd1d80d87e97ddf4bdcf4fc44d4
Raveesh Samkaria <rishu9417@gmail.com>
2020-09-11 12:46:16 +0530

    Added : Set the win limit to 21 & shows the won difference

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -11,5 +11,39 @@ do
 	else
 		tail=$(( $tail + 1 ))
 	fi
+	if [[ $head -eq 21 || $tail -eq 21 ]]
+	then
+		c=$(( $head - $tail ))
+		b=$(( $tail - $head ))
+		if [[ $c -ge 2 || $b -ge 2 ]]
+		then
+			break
+		else
+			a=`echo $(($RANDOM%2))`
+			if [[ $a -eq 0 ]]
+		        then
+                		head=$(( $head + 1 ))
+        		else
+                		tail=$(( $tail + 1 ))
+        		fi
+			if [[ $head -eq $tail ]]
+			then
+				echo -e "Tie\nHead=$head\nTail=$tail"
+				exit
+			elif [[ $head > $tail ]]
+			then
+				head=$(( $head - 1 ))
+				break
+			else
+				tail=$(( $tail - 1 ))
+				break
+			fi
+		fi
+	fi
 done
-echo -e "Head won $head times\nTail won $tail times"
+if [[ $head -gt $tail ]]
+then
+	echo -e "Head:$head\nTail:$tail\nHead won by $c times"
+else
+	echo -e "Head:$head\nTail:$tail\nTail won by $b times"
+fi
